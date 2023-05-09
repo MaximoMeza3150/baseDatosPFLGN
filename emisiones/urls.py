cfcc15258feb5c620d15dd57941994426082a250
MaximoMeza3150 <75499931+MaximoMeza3150@users.noreply.github.com>
2023-05-09 11:47:20 -0500

    registrar emisiones

--- emisiones/urls.py
+++ emisiones/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('emisionesFugitivas', views.mainEmisiones, name='emisionesPrincipal')
+    path('emisionesFugitivas', views.mainEmisiones, name='emisionesPrincipal'),
+    path('registrarEmisiones', views.registrarEmisiones, name='registrarEmisiones'),
 ]
--- emisiones/views.py
+++ emisiones/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def mainEmisiones(request):
     return render (request, 'mainEmisiones.html',{})
+
+def registrarEmisiones(request):
+    return render (request, 'registrarEmisiones.html',{})
