b4deb1508b6bad888070d30b3d38c519bc3229eb
MaximoMeza3150 <75499931+MaximoMeza3150@users.noreply.github.com>
2023-06-10 01:25:38 -0500

    creacion de usuarios

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uodcozk14e4ct@m=m*e6eb+!59tm4&e0t6q9j+8y!mb%o*x9(%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 CSRF_TRUSTED_ORIGINS = ['https://basededatospflgn.up.railway.app/']
-ALLOWED_HOSTS = ['https://basededatospflgn.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     'https://basededatospflgn.up.railway.app/',
 ]
