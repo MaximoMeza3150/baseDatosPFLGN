5f3eb099782e838556fa81020973b3af885497ce
MaximoMeza3150 <75499931+MaximoMeza3150@users.noreply.github.com>
2023-06-19 17:05:14 -0500

    cambio de pdfs

--- equipos/static/js/todosLosEquipos.js
+++ equipos/static/js/todosLosEquipos.js
@@ -1,25 +1,25 @@
 
 var listaEquipos = [{
         nombre: 'TKBJ-3000 - Tanque de condensado',
-        enlace : '../pdf/01_Tanques/TKBJ-3000'},
+        enlace :  '/static/pdf/01_Tanques/TKBJ-3000.pdf'},
 
         {nombre: 'TKBJ-3005 - Tanque de propano',
-        enlace : '../pdf/01_Tanques/TKBJ-3005'},
+        enlace : '/static/pdf/01_Tanques/TKBJ-3005.pdf'},
         
         {nombre: 'TKBJ-3010 - Tanque de propano/butano',
-        enlace: "https://drive.google.com/file/d/1LMnJ6GaoWfZxrleWc_exvqU6S_panc0o/preview"},
+        enlace: '/static/pdf/01_Tanques/TKBJ-3010.pdf'},
 
         {nombre: 'TKBJ-3020 - Tanque de nafta',
-        enlace: "https://drive.google.com/file/d/1n_eGdCzPNjW4TU3AaMpE1tABQcmttl7j/preview"},
+        enlace: '/static/pdf/01_Tanques/TKBJ-3020.pdf'},
         
         {nombre: 'TKBJ-3030 - Tanque de diesel',
-        enlace: "https://drive.google.com/file/d/1WAX3PRAXCJe9P0ItvfRG40OqEuHAGjKz/preview"},
+        enlace: '/static/pdf/01_Tanques/TKBJ-3030.pdf'},
 
         {nombre: 'TKBJ-3050 - Tanque de agua contraincendio',
-        enlace: "https://drive.google.com/file/d/1hKY5zGeXpK0znWg3fq3QLJIjH4Zk4VeQ/preview"},
+        enlace: '/static/pdf/01_Tanques/TKBJ-3050.pdf'},
 
         {nombre: 'VBA-3105 - Esfera de NGL',
-        enlace: 'https://drive.google.com/file/d/1q4Mi0NjQjyjLm1NKL8ZNgNu8qjavBpAK/preview'},
+        enlace: '/static/pdf/01_Tanques/VBA-3105.pdf'},
 
 ]
 
